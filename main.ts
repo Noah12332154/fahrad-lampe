49e8a70bae8532c862240355b27212037aed033b
Noah12332154 <157375383+Noah12332154@users.noreply.github.com>
2024-02-02 09:26:27 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        `)
+})
+basic.forever(function () {
+    if (true) {
+    	
+    } else {
+    	
+    }
+})
